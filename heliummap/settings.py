b47568af54decd61ec594b2f69d8df8029fca3bb
Karlo Tevzadze <mr.ktevzadze@gmail.com>
2021-12-27 11:16:12 +0400

    d

--- heliummap/settings.py
+++ heliummap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
+django_heroku.settings(locals())
